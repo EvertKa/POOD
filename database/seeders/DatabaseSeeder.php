1245f1be620eb1c74e8e8b6f9fcb6b90138aaca5
EvertKa <evert.karp@ametikool.ee>
2024-06-14 19:28:12 +0300

    done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +12,85 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'type' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => 'admin',
+            'created_at' => '2024-05-28 06:17:00',
+            'updated_at' => '2024-05-28 06:17:00',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\User::factory()->create([
+            'name' => 'user',
+            'type' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => 'user',
+            'created_at' => '2024-05-28 06:17:00',
+            'updated_at' => '2024-05-28 06:17:00',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Pablo',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Killa',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Grant',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Kuldne Grant',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Velo',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Tups',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Fox',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Grant Extreme',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
+        ]);
+
+        \App\Models\Item::factory()->create([
+            'name' => 'Lyft',
+            'description' => '10mg',
+            'price' => '5.00€',
+            'round_desc' => 'Tups',
         ]);
     }
 }
